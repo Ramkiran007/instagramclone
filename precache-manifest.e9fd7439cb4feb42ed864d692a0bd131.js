b73b5eb8bdf89f6326144e7c8e916471e9e0f856
Ramkiran <ramkiranmeduri.3@gmail.com>
2020-10-14 13:51:03 +0530

    Updates

---
+++ precache-manifest.e9fd7439cb4feb42ed864d692a0bd131.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65b4e3c073a5033d961f2fdede43c11f",
+    "url": "/instagramclone/index.html"
+  },
+  {
+    "revision": "ce54e52a3a9ffd1da9a2",
+    "url": "/instagramclone/static/css/main.ce4eafca.chunk.css"
+  },
+  {
+    "revision": "1bbdd586e09e6f600c53",
+    "url": "/instagramclone/static/js/2.1edd0dc0.chunk.js"
+  },
+  {
+    "revision": "b013b7cd7a2da92c16a2d9932cfeb76c",
+    "url": "/instagramclone/static/js/2.1edd0dc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce54e52a3a9ffd1da9a2",
+    "url": "/instagramclone/static/js/main.16c8f706.chunk.js"
+  },
+  {
+    "revision": "da63dd081a52bcb6eec2",
+    "url": "/instagramclone/static/js/runtime-main.dccb8d5f.js"
+  }
+]);
